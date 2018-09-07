6c9362cc18efce3134516855d3632c0eb50972f1
perlancar (on pc-office) <perlancar@gmail.com>
2018-09-07 20:41:03 +0700

    [bugfix] There's a hard limit on number of rows returned by GetSecuritiesStock endpoint, iterate to get all results

--- lib/Finance/SE/IDX.pm
+++ lib/Finance/SE/IDX.pm
@@ -77,19 +77,30 @@ sub list_idx_firms {
     my $sector = $args{sector} // '';
     my $board  = $args{board} // '';
 
-    my $res = _get_json("${urlprefix}StockData/GetSecuritiesStock?code=&sector=$sector&board=$board&draw=3&columns[0][data]=Code&columns[0][name]=&columns[0][searchable]=true&columns[0][orderable]=false&columns[0][search][value]=&columns[0][search][regex]=false&columns[1][data]=Code&columns[1][name]=&columns[1][searchable]=true&columns[1][orderable]=false&columns[1][search][value]=&columns[1][search][regex]=false&columns[2][data]=Name&columns[2][name]=&columns[2][searchable]=true&columns[2][orderable]=false&columns[2][search][value]=&columns[2][search][regex]=false&columns[3][data]=ListingDate&columns[3][name]=&columns[3][searchable]=true&columns[3][orderable]=false&columns[3][search][value]=&columns[3][search][regex]=false&columns[4][data]=Shares&columns[4][name]=&columns[4][searchable]=true&columns[4][orderable]=false&columns[4][search][value]=&columns[4][search][regex]=false&columns[5][data]=ListingBoard&columns[5][name]=&columns[5][searchable]=true&columns[5][orderable]=false&columns[5][search][value]=&columns[5][search][regex]=false&start=0&length=1000&search[value]=&search[regex]=false");
-    return $res unless $res->[0] == 200;
     my @rows;
-    for my $row0 (@{ $res->[2]{data} }) {
-        my $listing_date = $row0->{ListingDate}; $listing_date =~ s/T.+//;
-        my $row = {
-            code  => $row0->{Code},
-            name  => $row0->{Name},
-            listing_date => $listing_date,
-            shares => $row0->{Shares},
-            board => $row0->{ListingBoard},
-        };
-        push @rows, $row;
+
+    # there's a hard limit of 150, let's be nice and ask 100 at a time
+    my $start = 0;
+    while (1) {
+        my $res = _get_json("${urlprefix}StockData/GetSecuritiesStock?code=&sector=$sector&board=$board&draw=3&columns[0][data]=Code&columns[0][name]=&columns[0][searchable]=true&columns[0][orderable]=false&columns[0][search][value]=&columns[0][search][regex]=false&columns[1][data]=Code&columns[1][name]=&columns[1][searchable]=true&columns[1][orderable]=false&columns[1][search][value]=&columns[1][search][regex]=false&columns[2][data]=Name&columns[2][name]=&columns[2][searchable]=true&columns[2][orderable]=false&columns[2][search][value]=&columns[2][search][regex]=false&columns[3][data]=ListingDate&columns[3][name]=&columns[3][searchable]=true&columns[3][orderable]=false&columns[3][search][value]=&columns[3][search][regex]=false&columns[4][data]=Shares&columns[4][name]=&columns[4][searchable]=true&columns[4][orderable]=false&columns[4][search][value]=&columns[4][search][regex]=false&columns[5][data]=ListingBoard&columns[5][name]=&columns[5][searchable]=true&columns[5][orderable]=false&columns[5][search][value]=&columns[5][search][regex]=false&start=$start&length=100&search[value]=&search[regex]=false");
+        return $res unless $res->[0] == 200;
+        for my $row0 (@{ $res->[2]{data} }) {
+            my $listing_date = $row0->{ListingDate}; $listing_date =~ s/T.+//;
+            my $row = {
+                code  => $row0->{Code},
+                name  => $row0->{Name},
+                listing_date => $listing_date,
+                shares => $row0->{Shares},
+                board => $row0->{ListingBoard},
+            };
+            push @rows, $row;
+        }
+        if (@{ $res->[2]{data} } == 100) {
+            $start += 100;
+            next;
+        } else {
+            last;
+        }
     }
     [200, "OK", \@rows, {'table.fields'=>[qw/code name listing_date shares board/]}];
 }
